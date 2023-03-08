1112d83ccf585a2fb8942be0a10e121f2e7ac0d3
Elvis <elvis.salabarria.3@gmail.com>
2023-03-08 00:38:18 -0400

    [FIX] delete access to json file

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
-import 'package:localize_and_translate/localize_and_translate.dart';
-
 import 'presentation/app.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await translator.init(
-    localeType: LocalizationDefaultType.device,
-    languagesList: <String>['data'],
-    assetsDirectory: 'data/',
-  );
+  // await translator.init(
+  //   localeType: LocalizationDefaultType.device,
+  //   languagesList: <String>['data'],
+  //   assetsDirectory: 'data/',
+  // );
 
   runApp(
-    LocalizedApp(
-      child: const App(),
-    ),
+    // LocalizedApp(
+    const App(),
   );
 }
